96a1c9576026bc4222dc80ec7ba634939230a912
brook <brook.wangk@gmail.com>
2024-01-05 15:15:40 +0800

    feat<sadb>:新增安装脚本 & 默认 alias 文件

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_URL="https://raw.githubusercontent.com/UncleBrook/sadb/main/sadb"
+CONFIG_URL="https://raw.githubusercontent.com/UncleBrook/sadb/main/.alias"
+INSTALL_DIR="/usr/bin"
+CONFIG_DIR="$HOME/sadb/"
+
+mkdir -p "$CONFIG_DIR"
+
+echo "Downloading..."
+
+curl -o "$INSTALL_DIR/sadb" "$SCRIPT_URL"
+curl -o "$CONFIG_DIR/.alias" "$CONFIG_URL"
+
+chmod +x "$INSTALL_DIR/sadb"
+
+echo "Initializing..."
+
+if ! grep -q ".env" "$HOME/.bashrc"; then
+    echo "source \"alias adb=sadb\"" >> "$HOME/.bashrc"
+fi
+
+echo "Install completed."
